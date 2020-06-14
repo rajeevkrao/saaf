4071897c4ff19824bfad778a317f3f92fc7a2df7
Rajeev <rajeevraok@gmail.com>
2020-06-15 00:10:27 +0530

    form on 3d page

---
+++ public/js/test.js
@@ -0,0 +1,20 @@
+			function submition(){
+				/*var flag=0;
+				$(".dropDown").each(function(){
+        			// Test if the div element is empty
+        			if($(this).val() !== "0") {
+            			flag=1;
+            			document.querySelector(".contact_form").reportValidity();
+        			}
+	    		});*/
+				alert('Thank you for your submission');
+				/*console.log(flag)
+				return false;*/
+			}
+
+			function onChngSel(self){
+			    if($(self).children("option:selected").val()=="other")
+			    	$(`#${$(self).attr("id")}Other`).css("display","block")
+			    else
+			    	$(`#${$(self).attr("id")}Other`).css("display","none")
+			}
